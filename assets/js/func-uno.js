01689f4380a278ddc06f434494a70021357cc28f
samjoeyang <samjoeyang@gmail.com>
2022-11-09 21:24:35 +0800

    update

--- assets/js/func-uno.js
+++ assets/js/func-uno.js
@@ -1,11 +1,17 @@
-document.write("<script language=javascript src = 'assets/js/jquery.min.js'></script>"); 
+// document.write("<script language = javascript src = 'assets/js/jquery.min.js' ></script>"); 
 !function () {
+    var website_title = "视频教程网站(DEMO)"
     var pageDate = new Date;
     var domain = window.location.host || "MOOC COURSE DEMO"
-
+    var chg_title = document.getElementsByTagName('title')[0].innerText
+    if (chg_title) {
+        chg_title += " - " + website_title
+    }
     // $("#copyright_host").text(domain.toUpperCase()).attr('href', window.location.origin || "#")
     // $("#copyright_year").text(pageDate.getFullYear())
+    document.getElementsByTagName('title')[0].innerText = chg_title
+    console.info("通过func-uno.js改变页面title标签内容", chg_title)
     document.getElementById('copyright_host').innerHTML = domain.toUpperCase()
     document.getElementById('copyright_host').setAttribute('href', window.location.origin || "#")
-    document.getElementById('copyright_year').innerHTML = pageDate.getFullYear()
+    document.getElementById('copyright_year').innerText = pageDate.getFullYear()
 }();
